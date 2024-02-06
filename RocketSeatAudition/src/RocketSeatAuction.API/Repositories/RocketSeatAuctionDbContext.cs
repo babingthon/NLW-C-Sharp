2dd3eebe3d0bcabe7072c14374cb89afdbc5128a
babingthon <babingthon.bandeira@gmail.com>
2024-02-06 10:26:24 -0300

    feat: criação do endpoint Auction, entidades de Auctions e Items e configurações iniciais do projeto.

---
+++ RocketSeatAudition/src/RocketSeatAuction.API/Repositories/RocketSeatAuctionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketSeatAuction.API.Entities;
+
+namespace RocketSeatAuction.API.Repositories;
+
+public class RocketSeatAuctionDbContext : DbContext
+{
+  public DbSet<Auction> Auctions { get; set; }
+      
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=H:\GitHub\NLW Expert - Trilha C#\RocketSeatAudition\src\RocketSeatAuction.API\DB\leilaoDbNLW.db");
+    }
+}
---
+++ RocketSeatAudition/src/RocketSeatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketSeatAuction.API.Entities;
+using RocketSeatAuction.API.Repositories;
+
+namespace RocketSeatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction Execute()
+    {
+        var repository = new RocketSeatAuctionDbContext();
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .First(); 
+              
+    }
+}
